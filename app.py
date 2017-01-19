805fb81d30584e665bdd6b3e560d5181f020fdb6
Aurynn Shaw <self@aurynn.com>
2017-01-19 11:01:42 +1100

    adds environment check for port

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import bottle
 import begin
+import os
 
 @bottle.route("/")
 def index():
-	return b"hello"
+    return b"hello"
 
 
 @begin.start
 def run(host="0.0.0.0", port=8080):
+    if os.environ.get(APP_PORT):
+        port = int(os.environ.get(APP_PORT))
     bottle.run(host=host, port=port)
