1b3efc30ce28615c21e6864693945e3e339fe0be
Aurynn Shaw <self@aurynn.com>
2017-01-19 11:03:02 +1100

    use a string instead of a constant

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def index():
 
 @begin.start
 def run(host="0.0.0.0", port=8080):
-    if os.environ.get(APP_PORT):
-        port = int(os.environ.get(APP_PORT))
+    if os.environ.get("APP_PORT"):
+        port = int(os.environ.get("APP_PORT"))
     bottle.run(host=host, port=port)
